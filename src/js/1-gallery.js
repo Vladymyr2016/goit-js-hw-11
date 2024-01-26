3dafc31c0a5f55336b9369fc286a7d0393dfcaa7
Vladymyr2016 <142669270+Vladymyr2016@users.noreply.github.com>
2024-01-26 19:18:47 +0100

    t1

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,92 +1,88 @@
 import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
-const images = [
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg',
-    description: 'Hokkaido Flower',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
-    description: 'Container Haulage Freight',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
-    description: 'Aerial Beach View',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
-    description: 'Flower Blooms',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
-    description: 'Alpine Mountains',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
-    description: 'Mountain Lake Sailing',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
-    description: 'Alpine Spring Meadows',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
-    description: 'Nature Landscape',
-  },
-  {
-    preview:
-      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
-    original:
-      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
-    description: 'Lighthouse Coast Sea',
-  },
-];
-
-const ulElem = document.querySelector('.gallery');
-const listCart = images.map(
-  ({ preview, original, description }) => ` <li class="gallery-item">
-  <a class="gallery-link" href="${original}">
-    <img
-      class="gallery-image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-    />
-      </a>
-      
-</li>
-  `
-);
-const listCart1 = listCart.join('');
-ulElem.innerHTML = listCart1;
-
-new SimpleLightbox('.gallery a', {
+const formEl = document.querySelector('.feedback-form');
+const listEl = document.querySelector('.gallery');
+const loader = document.querySelector('.loader');
+const modal = new SimpleLightbox('.gallery a', {
   captionDelay: 250,
   captions: true,
   captionsData: 'alt',
   captionPosition: 'bottom',
 });
+function fetchImage(q) {
+  const API_KEY = '42040031-47a3b216d4f97a43df3da958a';
+  const PARAMS = new URLSearchParams({
+    key: API_KEY,
+    q,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+  });
+  const BASE_URL = 'https://pixabay.com/api';
+  const url = `${BASE_URL}/?${PARAMS}`;
+  return fetch(url).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
+}
+
+formEl.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+  loader.classList.remove('is-hidden');
+  listEl.innerHTML = '';
+  const serchQuery = e.currentTarget.elements.search.value.trim();
+  fetchImage(serchQuery)
+    .then(response => {
+      if (response.hits.length === 0) {
+        iziToast.error({
+          position: 'center',
+          title: 'Error',
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+        });
+      }
+      listEl.innerHTML = markUp(response.hits);
+      modal.refresh();
+    })
+    .catch(error => console.log(error))
+    .finally(() => {
+      loader.classList.add('is-hidden');
+    });
+}
+
+function markUp(arr) {
+  return arr
+    .map(
+      ({ webformatURL, largeImageURL, tags, comments, likes, downloads }) => {
+        return `<li> 
+     <a href='${largeImageURL}'><img src="${webformatURL}" alt="" /></a>
+      <h2>comments</h2>
+      <p>${comments}</p>
+      <h2>tags</h2>
+      <p>${tags}</p>
+            <h2>comments</h2>
+      <p>${likes}</p>
+      <h2>downloads</h2>
+      <p>${downloads}</p>
+      </li>`;
+      }
+    )
+    .join('');
+}
+
+// const markUp = data
+//   .map(country => {
+//     return `<li>
+//         <p>${country.name.common}</p>
+//         <img src="${country.flags.png}" alt="${country.flags.alt}">
+//         </li>`;
+//   })
+//   .join('');
+// ulElem.innerHTML = markUp;
